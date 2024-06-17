601e4968cd1fd37d1ba56a813c264672c89b4e2e
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-17 20:33:29 +0900

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,56 +23,31 @@ urlpatterns = [
     path('admin/',
          admin.site.urls),
     path('djangoapp/',
-         include(
-             'djangoapp.urls'
-         )
-        ),
-    path('', TemplateView.as_view(
-             template_name=
-             "Home.html")
-        ),
+         include('djangoapp.urls')),
+    path('',
+         TemplateView.as_view(
+             template_name="Home.html")),
     path('about/',
          TemplateView.as_view(
-             template_name=
-             "About.html"
-         )
-        ),
+             template_name="About.html")),
     path('contact/',
          TemplateView.as_view(
-             template_name=
-             "Contact.html"
-         )
-        ),
+             template_name="Contact.html")),
     path('login/',
          TemplateView.as_view(
-             template_name=
-             "index.html"
-         )
-        ),
+             template_name="index.html")),
     path('register/',
          TemplateView.as_view(
-             template_name=
-             "index.html"
-         )
-        ),
+             template_name="index.html")),
     path("dealers/",
          TemplateView.as_view(
-             template_name=
-             "index.html"
-         )
-        ),
+             template_name="index.html")),
     path("dealer/<int: dealer_id>",
          TemplateView.as_view(
-             template_name=
-             "index.html"
-         )
-        ),
+             template_name="index.html")),
     path("postreview/<int: dealer_id>",
          TemplateView.as_view(
-             template_name=
-             "index.html"
-         )
-        ),
+             template_name="index.html")),
 ] + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
