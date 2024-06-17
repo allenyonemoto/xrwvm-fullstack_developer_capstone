712bdc71cf8bb218528fb08bcd9481e8c9331e39
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-18 07:55:25 +0900

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -58,6 +58,7 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+
 def logout_request(request):
     logout(request)
     data = {"userName": ""}
@@ -65,6 +66,7 @@ def logout_request(request):
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
+
 def registration(request):
     data = json.loads(request.body)
     username = data['userName']
@@ -104,6 +106,7 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
+
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
@@ -115,6 +118,7 @@ def get_dealerships(request, state="All"):
          "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
+
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if dealer_id:
@@ -134,6 +138,7 @@ def get_dealer_reviews(request, dealer_id):
              "message": "Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
+
 def get_dealer_details(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
@@ -147,6 +152,7 @@ def get_dealer_details(request, dealer_id):
              "message": "Bad Request"})
 
 # Create a `add_review` view to submit a review
+
 def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
