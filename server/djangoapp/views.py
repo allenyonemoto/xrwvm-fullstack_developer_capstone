83ca58e2f2f137d885c036f5e0099f73d687fb70
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-17 10:37:14 +0900

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,6 +22,17 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -121,14 +132,3 @@ def add_review(request):
             return JsonResponse({"status": 401, "message": e})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
-
-def get_cars(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if(count == 0):
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
