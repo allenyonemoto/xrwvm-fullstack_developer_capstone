4ad666ab1be44210e4c930330f8a57db62a45488
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-17 19:20:27 +0900

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,60 +20,60 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', 
-         admin.site.urls), 
-    path('djangoapp/', 
+    path('admin/',
+         admin.site.urls),
+    path('djangoapp/',
          include(
              'djangoapp.urls'
          )
-        ), 
+        ),
     path('', TemplateView.as_view(
-        template_name = 
+        template_name=
         "Home.html")
-        ), 
-    path('about/', 
+        ),
+    path('about/',
          TemplateView.as_view(
-             template_name = 
+             template_name=
              "About.html"
          )
-        ), 
-    path('contact/', 
+        ),
+    path('contact/',
          TemplateView.as_view(
-             template_name = 
+             template_name=
              "Contact.html"
          )
-        ), 
-    path('login/',  
-         TemplateView.as_view(
-             template_name = 
-             "index.html"
-         )
-        ), 
-    path('register/', 
-         TemplateView.as_view(
-             template_name = 
-             "index.html"
-         )
-        ), 
-    path("dealers/", 
-         TemplateView.as_view(
-             template_name = 
-             "index.html"
-         )
-        ), 
-    path("dealer/<int: dealer_id>", 
+        ),
+    path('login/',
          TemplateView.as_view(
              template_name=
              "index.html"
          )
-        ), 
-    path("postreview/<int: dealer_id>", 
+        ),
+    path('register/',
          TemplateView.as_view(
-             template_name =
+             template_name=
              "index.html"
          )
-        ), 
+        ),
+    path("dealers/",
+         TemplateView.as_view(
+             template_name=
+             "index.html"
+         )
+        ),
+    path("dealer/<int: dealer_id>",
+         TemplateView.as_view(
+             template_name=
+             "index.html"
+         )
+        ),
+    path("postreview/<int: dealer_id>",
+         TemplateView.as_view(
+             template_name=
+             "index.html"
+         )
+        ),
 ] + static(
-    settings.STATIC_URL, 
+    settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
