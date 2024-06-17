9e2cab85a67e62357311157dc24d32ea4dae56f8
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-18 07:54:35 +0900

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,6 +12,8 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+
+
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
@@ -30,6 +32,7 @@ def get_request(endpoint, **kwargs):
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -42,6 +45,7 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 # Add code for posting review
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
