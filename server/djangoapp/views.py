96a93cba2fb78400cd99d41ad875ea8027cb0257
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-17 19:44:09 +0900

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-#from django.http import HttpResponseRedirect, HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-#from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-#from django.contrib import messages
-#from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -25,19 +25,19 @@ logger = logging.getLogger(__name__)
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0 ):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
         cars.append(
-            {"CarModel": 
-             car_model.name, 
-             "CarMake": 
+            {"CarModel":
+             car_model.name,
+             "CarMake":
              car_model.car_make.name
             }
         )
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
 
 
 # Create a `login_request` view to handle sign in request
@@ -54,7 +54,7 @@ def login_user(request):
         #  If user is valid, call login method to login current user
         login(request, user)
         data = {
-            "userName": username, 
+            "userName": username,
             "status": "Authenticated"
         }
     return JsonResponse(data)
@@ -62,7 +62,7 @@ def login_user(request):
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
@@ -82,17 +82,17 @@ def registration(request):
     except Exception as e:
         # If not, simply log this is a new user
         logger.debug(
-            "{} is new user".format(username), 
+            "{} is new user".format(username),
             e)
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(
-            username = username,
-            first_name = first_name, 
-            last_name = last_name, 
-            password = password, 
-            email = email
+            username=username,
+            first_name= first_name,
+            last_name =last_name,
+            password=password,
+            email=email
         )
         # Login the user and redirect to list page
         login(request, user)
